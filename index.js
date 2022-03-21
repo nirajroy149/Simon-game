61e492b678637b81b385c185a205320e14bbfdf8
Niraj Kumar Roy <nirajroy149@gmail.com>
2022-03-21 18:56:36 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,77 @@
+
+var started = "false";
+var gameColor = ["green", "red", "yellow", "green"]
+var gamePattern = [];
+var userPattern = [];
+var level = 0;
+
+$(document).on("keydown", function () {
+    $(".heading").text("Level " + level);
+    nextsequence();
+    started = "true";
+});
+
+function nextsequence() {
+    userPattern = [];
+
+    level++;
+    $("h1").text("Level " + level);
+
+    let randomColor = Math.floor(Math.random() * 4);
+    let color = gameColor[randomColor];
+    gamePattern.push(color);
+    $("#" + color).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+    playSound(color);
+}
+
+$("button").click(function () {
+
+    var userChosenColor = this.id;
+    userPattern.push(userChosenColor);
+
+    playSound(userChosenColor);
+    animatePress(userChosenColor);
+
+    checkCurrentColor(userPattern.length - 1);
+
+});
+
+function checkCurrentColor(currentlevel) {
+    if (gamePattern[currentlevel] === userPattern[currentlevel]) {
+        console.log("success");
+        if (gamePattern.length === userPattern.length) {
+            setTimeout(function () {
+                nextsequence();
+            }, 1000);
+        }
+    }
+    else{
+        console.log("wrong");
+        $("body").addClass("game-over");
+        setTimeout(function(){
+            $("body").removeClass("game-over");
+        }, 200);
+        $(".heading").text("Game Over, Press any key to Restart");
+        startover();
+    }
+
+}
+
+function startover(){
+    level = 0;
+    started = "false";
+    gamePattern = [];
+}
+
+function animatePress(name) {
+    $("#" + name).addClass("pressed");
+    setTimeout(function () {
+        $("#" + name).removeClass("pressed");
+    }, 100);
+}
+
+function playSound(name) {
+    var audio = new Audio();
+    audio.src = "./sounds/" + name + ".mp3"
+    audio.play();
+}
